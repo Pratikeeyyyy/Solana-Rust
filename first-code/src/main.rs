15f02b070fb0368dd0d163a79661220925dfcc9c
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-08-16 00:07:44 +0545

    fucntion types and immutability

--- first-code/src/main.rs
+++ first-code/src/main.rs
@@ -1,24 +1,82 @@
-// tuples : A is like a mixed box that can hold different types of things together! like char num boolean etc.
+// function and its types
+ // public function (pub  is used to make a function public) and default function is itself automatic private function.
 
  fn main(){
-
-    let person=("ram", 1, true);
-
-        // Access items using .0, .1, .2 (starts at 0)
-        println!("Name  :{}" , person.0);
-        println!("Rollno:{}" , person.1);
-        println!("student:{}" , person.2);
+    println!("{}", is_even(2));
  }
- 
-      // Empty tuple (called "unit")
-    let empty = ();
 
-    // With 2 items
-    let pair = ("hello", 42);
+ pub fn is_even(num: u8)-> bool{
+    let digit: u8 = num % 2;
+    digit ==0
+ }
 
-    // With 3 items  
-    let triple = (10, 20.5, "world");
+ // Public function
+pub fn public_fn() {}
 
-    // Destructuring (unpacking)
-    let (x, y, z) = triple;
-    println!("{} {} {}", x, y, z);  // → 10 20.5 world
+// Public struct with private fields
+pub struct User {
+    pub name: String,    // Public field
+    age: i32,            // Private field
+}
+
+// Public enum (all variants are automatically public)
+pub enum Color {
+    Red,
+    Blue,
+}
+
+// Public trait
+pub trait Animal {
+    fn make_sound(&self);
+}
+
+
+//  mutability:means can something change? In Rust, everything is immutable (can't change) by default. You have to explicitly say you want something to be mutable.
+
+ fn main(){
+    let  mut number = 5;
+    let number = 1;
+    println!("Number is :{}",number)
+ }
+
+fn main() {
+    // IMMUTABLE array can't change
+    let numbers = [1, 2, 3];
+    numbers[0] = 10;  // ❌ ERROR! Can't change
+    
+    // MUTABLE array  can change
+   
+    let mut numbers = [1, 2, 3];
+    numbers[0] = 10;  // ✅ Allowed!
+    println!("{:?}", numbers);  // → [10, 2, 3]
+}
+
+fn main() {
+    // IMMUTABLE tuple  can't change
+    let person = ("Alice", 25);
+    person.1 = 30;  // ❌ ERROR!
+    
+    // MUTABLE tuple  can change
+    let mut person = ("Alice", 25);
+    person.1 = 30;  // ✅ Allowed!
+    println!("{:?}", person);  // → ("Alice", 30)
+}
+
+
+// Variable (can be mutable or immutable)
+let x = 5;           // Immutable
+let mut y = 10;      // Mutable
+
+fn main() {
+    // This is SHADOWING (creating new variable)
+    const x = 5;
+    const x = x + 1;    // New variable, old one is gone
+    const x = x * 2;
+    println!("{}", x);  // → 12
+    
+    // This iss MUTABILITY (changing existing variable)
+    const mut y = 5;
+    y = y + 1;        // Changing the same variable
+    y = y * 2;
+    println!("{}", y);  // → 12
+}
