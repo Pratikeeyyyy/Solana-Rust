1601113b9b6513e87d3d7ab4e55a812dec96a8c9
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-08-15 18:37:09 +0545

    array and types and using them

--- first-code/src/main.rs
+++ first-code/src/main.rs
@@ -1,44 +1,14 @@
+fn main (){
+   //simple array
+    let array :[u8; 3]=[1, 2, 3]; // u8type of  array of 3 item
+    let array_2:[u8; 5]=[100; 5]; // u8 type of array where each item has item 100 up to 5th place
+    println!("index:{}, length:{}", array[0], array_2.len());
 
+    // printing the structure of array and other objects
+    println!("{:?}", array); // pprint whole array
+    println!("{}", array[1]);// print specific item of resepctive index
+    println!("{}", array[0]);
+    println!("{}", array[2]);
+println!("{}", array_2[4]);
 
-// 🦀 RUST IS STRICTLY/STATICALLY TYPED
-
-//variables:
-fn main(){
-    //unsigned integer
-    // u8, u16 , u 32 ,u64 etc
-    let unsigned: u16= 10;
-
-    //signed integer
-    // i8, i16, i32, i64
-    let signed: i8 = -12;
-
-    //float is used for decimal and it accepts only decimal that must have .0
-    let float : f32 =1.0;
-
-    println!("unsign: {} sign:{} float {} ", unsigned, signed , float);
-    
-
-    //char can only be
-
-    // Strings (use double quotes "")
-    let name: &str = "Pratik";      // &str = string slice
-    let car: String = String::from("Pagani");  // String = owned string
-    let emoji  :char ='🚀';
-
-    println!("Letter: {}, car: {} , emoji{}", name, car , emoji);
-    
-// For boooleans
-    let is_true: bool =true;
-    println!(":isTrue :{}", is_true);
 }
-
-// ie simple terms
-// let x: u32 = 10;        // u32 = unsigned 32-bit integer
-// let y: i32 = -5;        // i32 = signed 32-bit integer
-// let z: char = 'A';      // char = single character
-// let name: &str = "Rust"; // &str = string slice
-// ❌ Wrong (Type mismatch)
-// rust
-// let x: u32 = -5;        // ERROR: u32 can't be negative
-// let y: char = "A";      // ERROR: char expects '' not ""
-// let z: String = "Rust"; // ERROR: &str != String
