7b8963285a9abb32311d110df9298f38e774dfbc
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-08-16 22:49:03 +0545

    slice  commands (r&u)

--- first-code/src/main.rs
+++ first-code/src/main.rs
@@ -1,21 +1,16 @@
 fn main(){
+let first : &str=" Hello everyone";
 
-    let array =[0, 1, 2, 3]; // array of 4 items
+let mut second: String= String::from ("How are you");
 
-    // Creating a slice from index 1 to 3 (excludes 3)
-    let slice =&array [1..3];// correct range of syntaxx from 1  to 2
+let slice =&second[..6];// slcie form index starting to 6( excluding 6)
+println!("slice: {}", slice);
+println!("slice length:{}", slice.len());
+second.push('1');
+// string.push_first("!");// old
+second.insert(0, '!');
+println!("After inserting : {}", second);
+second= second.replace("How", "Guys");
+println!("After inserting : {}", second);
 
-    //passing array refrence and sllice 
-    borrowing_slice(&array, slice);
 }
-
-// First parameter: &[u8] (borrowed slice of the whole array)
-// Second parameter: &[u8] (borrowed slice of part of array)
- fn borrowing_slice(array: &[u8], slice: &[u8]){
-    println!("array:{:?}", array);
-    println!("slice:{:?}", slice);
-    println!("lenght of slice: {}", slice.len());
-    println!("First two element of slice {},{}", slice[0], slice[1]);
-
-
- }
