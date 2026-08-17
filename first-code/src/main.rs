e2b1a62fe0ef198c10ceeb6eaed228d36e8de6fb
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-08-17 23:22:27 +0545

    match statement

--- first-code/src/main.rs
+++ first-code/src/main.rs
@@ -1,30 +1,44 @@
+// match compares a value against multiple patterns and executes the code for the first matching pattern.
+
+
+
 fn main(){
-   // for loop 
-//  let n=10;
-//  if n>0{
-//     println!("It is greater than 0");
-//  }
-// else if n<0{
-//     println!("It is less than 0");
-// }
-// else {
-//     println!("The number is 0");
-// }
-
-// for loop similar to python
-// for i in 0..7{
-//     println!("{}" , i);
-// }
-
-
-// while loop
-let mut  i = 0;
-while i<6{
-    println!("{}", i);
-    i+= 1;
-    if i ==3{
-        println!("exit");
-        break // break or  contine to terminate the execution
-    }
+let i =7;
+match i {
+    0=> println!("0"),
+    1|2|3 => println!("1,2"),
+    4..=5=> println!("***"),
+    _=> println!("default")
 }
+
+// eg 2
+   let is_raining = true;
+    
+    match is_raining {
+        true => println!("Bring an umbrella! ☂️"),
+        false => println!("Enjoy the sunshine! ☀️"),
+    }
+
+    // eg3
+      let score = 85;
+    
+    match score {
+        0..=59 => println!("Grade: F"),
+        60..=69 => println!("Grade: D"),
+        70..=79 => println!("Grade: C"),
+        80..=89 => println!("Grade: B"),
+        90..=100 => println!("Grade: A"),
+        _ => println!("Invalid score!"),
+    }
+    // Output: Grade: B
+
+    // eg4
+    let name = "Alice";
+    
+    match name {
+        "Alice" => println!("Hello Alice!"),
+        "Bob" => println!("Hi Bob!"),
+        _ => println!("Who are you?"),
+    }
+    // Output: Hello Alice!
 }
