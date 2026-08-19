2299d74e003185ece77920123df03e07d295ff36
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-08-19 18:07:53 +0545

    Vector

--- first-code/src/main.rs
+++ first-code/src/main.rs
@@ -1,27 +1,11 @@
-// Enu = Short for Enumeration = A way to define a type by listing all possible values it can be.
-// like a dropdowm nenu with fixed options.
-
-#[derive (Debug)]
-enum Myenum{
-    A,
-    B(i32),
-    C{x:i32, y:i32}
-}
+// vector: Vector = A growable array that can hold multiple values of the SAME type.
+// Think of it like a shopping list — you can add, remove, and access items, and it automatically grows as you add more!
 
 fn main(){
-    let a: Myenum = Myenum:: A;
-    let b: Myenum = Myenum:: B(5);
-    let c: Myenum = Myenum :: C{x: 10 , y:20};
-    println!("{:?}",a);
-    println!("{:?}",b);
-    println!("{:?}" ,c);
-
-
-
-if let Myenum ::B(value)= b {
-    println!("{}", value);
-}
-if let Myenum ::C{x,y}=c{
-    println!("{} {}" , x,y)
-}
+let mut vec: Vec<i64> = vec![1,2,3,4,5,6];
+ vec.len();
+ vec[0];
+ vec.push(6);
+ vec.remove(0);
+ println!("{:?}", vec);
 }
